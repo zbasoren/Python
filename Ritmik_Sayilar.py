5624dfa26d756bce14b5bb5ce7f482ee8f7e9f44
zbasoren <154058276+zbasoren@users.noreply.github.com>
2023-12-17 22:47:35 +0300

    Update Ritmik_Sayilar.py

--- Ritmik_Sayilar.py
+++ Ritmik_Sayilar.py
@@ -1,3 +1,4 @@
+# 1'den 10'a kadar olan tüm sayıların 100'e kadar olan ritmik sayılar tablosunu iç-içe döngü yapılarını kullanarak python dilinde kodlamasıdır.
 for i in range(1, 11):
     print("-"*20)
     for y in range(0, 101, i):
