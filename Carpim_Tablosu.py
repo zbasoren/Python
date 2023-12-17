8b7905373a9878676da2571709f7efec913d3ae1
zbasoren <154058276+zbasoren@users.noreply.github.com>
2023-12-17 17:50:20 +0300

    Add files via upload

---
+++ Carpim_Tablosu.py
@@ -0,0 +1,5 @@
+## Çarpım tablosu yapımı##
+for i in range(1, 10):
+    print("-" * 20) ### aralarına çizgi koyması için yazıldı.Güzel bir görüntü için.
+    for y in range(1, 10):
+        print(i, "*", y, "=", i * y)
