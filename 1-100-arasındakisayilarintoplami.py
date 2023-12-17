a840e143e3cd21b1fe9b1311def36c0592183ab1
zbasoren <154058276+zbasoren@users.noreply.github.com>
2023-12-17 14:58:50 +0300

    Add files via upload

---
+++ 1-100-arasındakisayilarintoplami.py
@@ -0,0 +1,18 @@
+# 1 ile 100 arasındali sayıların toplamını bulma
+#Matematik formülü ile bulmak
+n = int(input("BİR SAYI GİRİNİZ :"))
+x = (n * (n + 1)) / 2
+print(x)
+
+## Veya ?
+### Kullanıcıdan sayı alarak verdiği sayıların toplamını bulmak
+m = int(input("Bir sayı giriniz :"))
+a=[]
+for i in range(1,m+1):
+    a.append(i)
+print("Sayılar Toplamı :",sum(a))
+
+a=[]
+for i in range(1,10):
+    a.append(i)
+print("sayılar toplamı",sum(a))
